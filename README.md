16ad3cdd664c0b41910849ce2e9e8950dc513d99
Miles Pei <mpei36@gmail.com>
2022-11-17 15:15:51 -0600

    Readme

--- README.md
+++ README.md
@@ -1,34 +1,34 @@
-(a) 
-    My algorithm's structure is based on the textbooks WhateverFirstSearch. A queue stores the positions to be checked. At any position, I check the 4 adjacent positions and add them to the queue if they are valid. A map to keeps track of the minimum number of moves taken to reach the current position. A visited set is used to store the positions that have already been checked.
+# Documentation
+
+(a)
+    My algorithm's structure is based on Dijkstra's shortest path algorithm. A queue stores the token positions to be checked. A visited set is used to store the positions that have already been checked.At any position, I check the 4 adjacent positions and add them to the queue if they are valid. A distance map then keeps track of the minimum number of moves taken to reach the current position. The algorithm terminates when the queue is empty or the destination is reached.
 
 (b)
-    n = 10
-        SmallF
-        Minimum number of moves:
-        Running time:
-
-        SmallE
-        Minimum number of moves:
-        Running time:
-
-    n = 100
-        MediumF
-        Minimum number of moves:
-        Running time:
-
-        MediumE
-        Minimum number of moves:
-        Running time:
-
     n = 1000
-        LargeF
-        Minimum number of moves:
-        Running time:
+        LargeE.txt
+        Shortest path cost:  55
+        Runtime: 51227.74243354797ms
 
-        LargeE
-        Minimum number of moves:
-        Running time:
+        LargeF.txt
+        Shortest path cost:  56
+        Runtime: 53568.77660751343ms
+    n = 100
+        MediumE.txt
+        Shortest path cost:  65
+        Runtime: 23.996829986572266ms
 
-(c) Requires Python 3.10.4
+        MediumF.txt
+        Shortest path cost:  64
+        Runtime: 16.066551208496094ms
+    n = 10
+        SmallE.txt
+        Shortest path cost:  8
+        Runtime: 0.0ms
+
+        SmallF.txt
+        Shortest path cost:  7
+        Runtime: 0.0ms
+
+(c) Requires Python 3.10.4 or higher
     Run the program with
         py hw6.py
--- hw6.py
+++ hw6.py
@@ -2,52 +2,49 @@ import os
 import re
 from time import time
 
-
-def printMaze(M, n):
-    for i in range(n):
-        for j in range(n):
-            if (i, j) == (0, 0):
-                print("#", end=" ")
-            else:
-                if M[i][j] == 0:
-                    print("  ", end=" ")
-                else:
-                    print(M[i][j], end=" ")
-        print()
-
-
-def mazeSolver(maze, n):
+def shortest_path(maze, n):
     end = (n - 1, n - 1)
     moves = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    queue, costs = [(0, 0)], {(0, 0): 0}
+    queue, dist = [(0, 0)], {(0, 0): 0}
 
     visited = set()
     while queue:
-        token = queue.pop(0)
-        x, y = token[0], token[1]
+        token = queue.pop(0)  # Get next token location
+        (x, y) = token
         if token == end:  # Goal reached
-            return costs[token] # Return shortest path cost
+            return dist[token]  # Return shortest path distance
 
         if token not in visited:
-            visited.add(token)
-            for dx, dy in moves: # Check all possible moves
+            visited.add(token)  # Mark as visited
+            for dx, dy in moves:  # Check all possible moves
                 new_x = x + dx * maze[x][y]  # New move in x direction
                 new_y = y + dy * maze[x][y]  # New move in y direction
-                if 0 <= new_x < n and 0 <= new_y < n: # Check if new move is valid
-                    if maze[new_x][new_y] != 0:  # Ignore 0s
+                if 0 <= new_x < n and 0 <= new_y < n:  # Check if new move is valid
+                    if maze[new_x][new_y] != 0:  # Ignore 0 locations
                         next = (new_x, new_y)  # Next token location
-                        if (next not in costs or costs[next] > costs[token] + 1):  # Better cost exists
-                            costs[next] = costs[token] + 1
-                            queue.append(next)
-    return -1
+                        # Undiscovered or better distance exists
+                        if (next not in dist or dist[next] > dist[token] + 1):
+                            dist[next] = dist[token] + 1  # Update distance
+                            queue.append(next)  # Add to queue
+
+    return -1  # Exuasted all possible moves and no solution found
 
 
 def main():
 
+    # Read input file
     path = os.getcwd() + "\\tests\\"
     testdir = os.listdir(path)
-    tests = [f for f in testdir if re.match(r"test.*\.txt", f)]
-
+    tests = None
+    print("Enter \"sample\" to run sample test cases, \"test\" to run answer test cases, or nothing to run all test cases.")
+    match input(""):
+        case "sample":
+            tests = [f for f in testdir if re.match(r"[^test].*\.txt", f)]
+        case "test":
+            tests = [f for f in testdir if re.match(r"test.*\.txt", f)]
+        case _:
+            tests = [f for f in testdir]
+    print("\n")
     for test in tests:
         temp = path + test
         print("Running: ", test)
@@ -56,11 +53,15 @@ def main():
             n = A[0]
             A = A[1:]
             M = [A[i: i + n] for i in range(0, len(A), n)]
-            move_cost = mazeSolver(M, n)
-            if move_cost == -1:
-                print("Output: No Solution")
+            start_time = time()
+            distance = shortest_path(M, n)
+            duration = time() - start_time
+            if distance == "-1":
+                print("No solution found.")
+                print("Runtime: "+str(duration*1000)+"ms\n")
             else:
-                print("Output: ", move_cost)
+                print("Shortest path distance: ", distance)
+                print("Runtime: "+str(duration*1000)+"ms\n")
 
 
 if __name__ == "__main__":
